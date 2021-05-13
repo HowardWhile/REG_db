b2f9131e057167644cbdc46dfb506e8863095c81
HOW_Dell <makubex49@gmail.com>
2021-05-13 09:18:56 +0800

    init

---
+++ MyAPP/REG_db.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIM.Modules
+{
+    class REG_db
+    {
+        private string db_path = "";
+        public REG_db(string iPath = "")
+        {
+            this.db_path = iPath;
+        }
+
+    }
+}
